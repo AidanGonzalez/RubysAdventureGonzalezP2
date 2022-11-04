01d3ed198512c231c51f0f3c13bd4808aed5413f
AidanGonzalez <724624@student.dusd.net>
2022-11-04 09:53:11 -0700

    Finished movement controls
    Finished movement controls for wasd and began tile mapping.

--- RubysAdventureGonzalez/Assets/Scripts/RubyController.cs
+++ RubysAdventureGonzalez/Assets/Scripts/RubyController.cs
@@ -8,15 +8,19 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Create 2 variables to use unity built in axes
         float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         Debug.Log(horizontal);
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
