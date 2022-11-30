c60fc530cf1c2ba30025ec1ee7e195cbafba0f22
AidanGonzalez <724624@student.dusd.net>
2022-11-30 09:54:40 -0800

    :p
    I did stuff (im very tired)

--- RubysAdventureGonzalez/Assets/Scripts/DamageZone.cs
+++ RubysAdventureGonzalez/Assets/Scripts/DamageZone.cs
@@ -2,23 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthCollectible : MonoBehaviour
+public class DamageZone : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Object that entered the trigger : " + other);
         RubyController controller = other.GetComponent<RubyController>();
+
         if (controller !=null)
         {
-            if (controller.currentHealth < controller.maxHealth)
-
-            {
-                controller.ChangeHealth(1);
-                Destroy(gameObject);
-            }
+            controller.ChangeHealth(-1);
         }
     }
-
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +24,4 @@ public class HealthCollectible : MonoBehaviour
     {
         
     }
-    
 }
-
--- RubysAdventureGonzalez/Assets/Scripts/RubyController.cs
+++ RubysAdventureGonzalez/Assets/Scripts/RubyController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float timeInvincible = 2.0f;
+    public int health { get { return currentHealth; } }
     public float speed = 5.0f;
-
+    bool isInvincible;
+    float invincibleTimer;
     public int maxHealth = 5;
-    int currentHealth;
+   public int currentHealth;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -36,13 +39,25 @@ public class RubyController : MonoBehaviour
         Vector2 position = rigidbody2d.position;
         position.x = position.x + speed * horizontal * Time.deltaTime;
         position.y = position.y + speed * vertical * Time.deltaTime;
-
         rigidbody2d.MovePosition(position);
     }
 
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
+
 }
